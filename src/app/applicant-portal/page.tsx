6eca81a5b7776a628132130da38faac9740282e3
Jsanchez767 <jsanchez@maticslab.com>
2025-11-29 22:41:08 -0600

    Remove unused applicant-portal page

--- src/app/applicant-portal/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ApplicantDashboard } from '@/components/ApplicationsHub/Applications/ApplicantPortal/ApplicantDashboard'
-
-export default function ApplicantPortalPage() {
-  return <ApplicantDashboard />
-}
