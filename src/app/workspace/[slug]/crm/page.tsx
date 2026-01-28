c0eb4d2e76e67535ccfc92e0f9da9c187e3ffeff
Jesus Sanchez <Jsanchez767@student.ccc.edu>
2026-01-27 21:43:10 -0600

    Fix NavigationLayout props in CRM page

--- src/app/workspace/[slug]/crm/page.tsx
+++ src/app/workspace/[slug]/crm/page.tsx
@@ -125,11 +125,7 @@ function CRMPageContent() {
   ).length
 
   return (
-    <NavigationLayout
-      workspaceId={workspace.id}
-      workspaceName={workspace.name}
-      workspaceSlug={workspace.slug}
-    >
+    <NavigationLayout workspaceSlug={workspace.slug}>
       <div className="p-6 space-y-6">
         {/* Header */}
         <div className="flex items-center justify-between">
